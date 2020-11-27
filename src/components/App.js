e3cfd95d21e282ee2f1269eee28a67bad6144f6d
Jing Ren <jren@zuora.com>
2020-11-27 16:00:30 +0800

    create new post with useMutation

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import '../styles/App.css';
 import {LinkList} from './LinkList'
 import React from "react";
+import {CreateLink} from "./CreateLink";
 
 function App() {
-    return <LinkList/>
+    return (
+        <div>
+            <CreateLink/>
+            <LinkList/>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/components/CreateLink.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react'
+import gql from 'graphql-tag'
+// import {useMutation} from "@apollo/react-hooks";
+import {useMutation} from 'react-apollo'
+
+export const CreateLink = () => {
+    const [url, setUrl] = useState('')
+    const [description, setDescription] = useState('')
+
+    const POST_MUTATION = gql`
+  mutation PostMutation($description: String!, $url: String!) {
+    post(description: $description, url: $url) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`
+    const [addPost, {data}] = useMutation(POST_MUTATION)
+    
+    const submit = (e) => {
+        e.preventDefault();
+        addPost({ variables: { url, description } })
+        setDescription('')
+        setUrl('')
+    }
+
+    return (
+        <div>
+            <div className="flex flex-column mt3">
+                <input
+                    className="mb2"
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                    type="text"
+                    placeholder="A description for the link"
+                />
+                <input
+                    className="mb2"
+                    value={url}
+                    onChange={e => setUrl(e.target.value)}
+                    type="text"
+                    placeholder="The URL for the link"
+                />
+            </div>
+            <button onClick={e => submit(e)}>Submit</button>
+        </div>
+    )
+}
